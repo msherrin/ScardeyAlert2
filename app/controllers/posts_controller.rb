99c4d0c5fecaf4462e44dde9c07222dbb0bc7a41
msherrin <msherrin@gmail.com>
2014-05-10 19:43:06 +1000

    Associated and nested topics and posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,20 +8,26 @@ class PostsController < ApplicationController
     # automagic: params = { controller => "Apartments", :id => 3 }
   	@post = Post.find(params[:id])
     # automagic render from views: render 'posts/show'
+    @topic =Topic.find(params[:topic_id])
   end
 
   def new
-  	@post = Post.new
+  	 @topic = Topic.find(params[:topic_id])
+     @post = Post.new
     # authorize @post
+    authorize @post
   end
 
   def create
+    @topic = Topic.find(params[:topic_id])
     # params ={ :post => {:title => 'yay', :body => "im a body"}}
     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    @post.topic = @topic
+
     authorize @post
   if @post.save
     flash[:notice] = "Post was saved."
-    redirect_to @post # I would want this from u as a beginner: post_path(post)   
+    redirect_to [@topic, @post], notice: "Post was saved successfully." # I would want this from u as a beginner: post_path(post)   
   else
     flash[:error] = "There was an error saving the post. Please try again."
     render :new
@@ -29,16 +35,18 @@ class PostsController < ApplicationController
 end
   
   def edit
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
   end
 
   def update
+    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize @post
     if @post.update_attributes(params.require(:post).permit(:title, :body))
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Bloccit::Application.routes.draw do
 
   devise_for :users
-  resources :posts
-  get 'archives', to: 'archives#index'
+
+  resources :users
+  
+  resources :topics do
+		resources :posts, except: [:index]
+	end
+
+	resources :posts, only: [:index, :new, :create]
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
